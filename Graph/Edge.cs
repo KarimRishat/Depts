d8792aff60bd461e5f3d7bcda9cfcd9fe08e6cd5
Rishat <karimr2000@mail.ru>
2021-11-03 00:58:35 +0300

    Добавьте файлы проекта.

---
+++ Graph/Edge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    class Edge
+    {
+        public Vertex start;
+        public Vertex end;
+        public int weight;
+        public Edge(Vertex from, Vertex to, int weight = 1)
+        {
+            this.start = from;
+            this.end = to;
+            this.weight = weight;
+        }
+    }
+}
